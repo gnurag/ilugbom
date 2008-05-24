1f50aeb30e5d3c928806a33288dbcacc081a947d
Anurag <anurag@xinh.org>
2008-05-24 09:57:34 +0000

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -5,60 +5,56 @@
 ActiveRecord::Schema.define() do
 
   create_table "articles", :force => true do |t|
-    t.column "title",      :string,    :limit => 150, :default => "",    :null => false
-    t.column "tags",       :string,    :limit => 200
-    t.column "author_id",  :integer,                                     :null => false
-    t.column "body",       :text,                     :default => "",    :null => false
-    t.column "created_at", :datetime,                                    :null => false
-    t.column "updated_at", :timestamp,                                   :null => false
-    t.column "published",  :boolean,                  :default => false, :null => false
+    t.column "title",      :string,   :limit => 150, :default => "",    :null => false
+    t.column "tags",       :string,   :limit => 200
+    t.column "author_id",  :integer,                                    :null => false
+    t.column "body",       :text,                    :default => "",    :null => false
+    t.column "created_at", :datetime,                                   :null => false
+    t.column "updated_at", :datetime,                                   :null => false
+    t.column "published",  :boolean,                 :default => false, :null => false
   end
 
   add_index "articles", ["author_id"], :name => "author_id"
   add_index "articles", ["published"], :name => "published"
   add_index "articles", ["title"], :name => "title"
-#  add_index "articles", ["body"], :name => "body"
 
   create_table "events", :force => true do |t|
-    t.column "title",       :string,    :limit => 150, :default => "", :null => false
-    t.column "agenda",      :text,                     :default => "", :null => false
-    t.column "description", :text,                     :default => "", :null => false
+    t.column "title",       :string,   :limit => 150, :default => "", :null => false
+    t.column "agenda",      :text,                    :default => "", :null => false
+    t.column "description", :text,                    :default => "", :null => false
     t.column "minute_id",   :integer
     t.column "venue_id",    :integer
     t.column "date",        :datetime
     t.column "speaker_id",  :integer
-    t.column "published",   :integer,   :limit => 4,   :default => 0,  :null => false
-    t.column "created_at",  :timestamp,                                :null => false
+    t.column "published",   :integer,  :limit => 4,   :default => 0,  :null => false
+    t.column "created_at",  :datetime,                                :null => false
   end
 
   add_index "events", ["minute_id"], :name => "minute_id"
   add_index "events", ["venue_id"], :name => "venue_id"
   add_index "events", ["speaker_id"], :name => "speaker_id"
   add_index "events", ["published"], :name => "published"
-#  add_index "events", ["description"], :name => "description"
-#  add_index "events", ["agenda"], :name => "agenda"
   add_index "events", ["title"], :name => "title"
 
   create_table "minutes", :force => true do |t|
-    t.column "event_id",   :integer,                                :null => false
-    t.column "author_id",  :integer,                                :null => false
-    t.column "body",       :text,                   :default => "", :null => false
-    t.column "published",  :integer,   :limit => 4, :default => 0,  :null => false
-    t.column "created_at", :timestamp,                              :null => false
+    t.column "event_id",   :integer,                               :null => false
+    t.column "author_id",  :integer,                               :null => false
+    t.column "body",       :text,                  :default => "", :null => false
+    t.column "published",  :integer,  :limit => 4, :default => 0,  :null => false
+    t.column "created_at", :datetime,                              :null => false
   end
 
   add_index "minutes", ["event_id"], :name => "event_id"
   add_index "minutes", ["author_id"], :name => "author_id"
   add_index "minutes", ["published"], :name => "published"
-#  add_index "minutes", ["body"], :name => "body"
 
   create_table "pages", :force => true do |t|
-    t.column "title",      :string,    :limit => 30, :default => "", :null => false
-    t.column "body",       :text,                    :default => "", :null => false
-    t.column "author_id",  :integer,                                 :null => false
-    t.column "order",      :integer,   :limit => 4
-    t.column "published",  :integer,   :limit => 4,  :default => 0,  :null => false
-    t.column "created_at", :timestamp,                               :null => false
+    t.column "title",      :string,   :limit => 30, :default => "", :null => false
+    t.column "body",       :text,                   :default => "", :null => false
+    t.column "author_id",  :integer,                                :null => false
+    t.column "order",      :integer,  :limit => 4
+    t.column "published",  :integer,  :limit => 4,  :default => 0,  :null => false
+    t.column "created_at", :datetime,                               :null => false
   end
 
   add_index "pages", ["title"], :name => "title"
@@ -67,16 +63,16 @@ ActiveRecord::Schema.define() do
   add_index "pages", ["published"], :name => "published"
 
   create_table "people", :force => true do |t|
-    t.column "username",   :string,    :limit => 50,  :default => "", :null => false
-    t.column "password",   :string,    :limit => 45,  :default => "", :null => false
-    t.column "fullname",   :string,    :limit => 100, :default => "", :null => false
-    t.column "nickname",   :string,    :limit => 50
-    t.column "irc_nick",   :string,    :limit => 30
-    t.column "email",      :string,    :limit => 100, :default => "", :null => false
-    t.column "webpage",    :string,    :limit => 100
-    t.column "visible",    :integer,   :limit => 4,   :default => 0,  :null => false
-    t.column "deleted",    :integer,   :limit => 4,   :default => 0,  :null => false
-    t.column "created_at", :timestamp,                                :null => false
+    t.column "username",   :string,   :limit => 50,  :default => "", :null => false
+    t.column "password",   :string,   :limit => 45,  :default => "", :null => false
+    t.column "fullname",   :string,   :limit => 100, :default => "", :null => false
+    t.column "nickname",   :string,   :limit => 50
+    t.column "irc_nick",   :string,   :limit => 30
+    t.column "email",      :string,   :limit => 100, :default => "", :null => false
+    t.column "webpage",    :string,   :limit => 100
+    t.column "visible",    :integer,  :limit => 4,   :default => 0,  :null => false
+    t.column "deleted",    :integer,  :limit => 4,   :default => 0,  :null => false
+    t.column "created_at", :datetime,                                :null => false
   end
 
   add_index "people", ["username"], :name => "username_2", :unique => true
@@ -97,17 +93,17 @@ ActiveRecord::Schema.define() do
   add_index "settings", ["key"], :name => "key"
 
   create_table "venues", :force => true do |t|
-    t.column "short_name",     :string,    :limit => 50,  :default => "", :null => false
-    t.column "name",           :string,    :limit => 150, :default => "", :null => false
-    t.column "organization",   :string,    :limit => 150, :default => "", :null => false
-    t.column "locality",       :string,    :limit => 50,  :default => "", :null => false
-    t.column "address",        :string,    :limit => 200, :default => "", :null => false
+    t.column "short_name",     :string,   :limit => 50,  :default => "", :null => false
+    t.column "name",           :string,   :limit => 150, :default => "", :null => false
+    t.column "organization",   :string,   :limit => 150, :default => "", :null => false
+    t.column "locality",       :string,   :limit => 50,  :default => "", :null => false
+    t.column "address",        :string,   :limit => 200, :default => "", :null => false
     t.column "reaching",       :text
-    t.column "venue_url",      :string,    :limit => 200
-    t.column "contact_person", :string,    :limit => 30
-    t.column "contact_phone",  :string,    :limit => 11
-    t.column "published",      :integer,   :limit => 4,   :default => 0,  :null => false
-    t.column "created_at",     :timestamp,                                :null => false
+    t.column "venue_url",      :string,   :limit => 200
+    t.column "contact_person", :string,   :limit => 30
+    t.column "contact_phone",  :string,   :limit => 11
+    t.column "published",      :integer,  :limit => 4,   :default => 0,  :null => false
+    t.column "created_at",     :datetime,                                :null => false
   end
 
   add_index "venues", ["address"], :name => "address"
