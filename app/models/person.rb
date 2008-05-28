d71c16c61392b89509fafc3206532f7ca0e33380
Anurag <anurag@xinh.org>
2008-05-28 22:36:45 +0000

    added method to authenticate users against db.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,12 @@ class Person < ActiveRecord::Base
   has_many :articles, :foreign_key => "author_id"
   has_many :minutes, :foreign_key => "author_id"
   has_many :pages, :foreign_key => "author_id"
+
+
+  def self.authenticate(username, password)
+    password = OpenSSL::Digest::SHA1.new(password).hexdigest
+    user     = Person.find(:first, :select => [:password], :conditions => ["username = ?", username])
+    return (user and user.password == password) ? true : false
+  end
+
 end
