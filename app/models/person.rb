83c69934ac7ddf93aa1d42c1d11208e687b4f666
Anurag <anurag@xinh.org>
2008-06-04 20:47:38 +0000

    Added a function to generate PipeSeparatedValue.

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,8 +6,26 @@ class Person < ActiveRecord::Base
 
   def self.authenticate(username, password)
     password = OpenSSL::Digest::SHA1.new(password).hexdigest
-    user     = Person.find(:first, :select => [:password], :conditions => ["username = ?", username])
-    return (user and user.password == password) ? true : false
+    user     = Person.find(:first, :conditions => ["username = ? AND deleted = 0", username])
+    return (user and user.password == password) ? user : false
   end
+
+  ## Get pipe separated value for a Person object
+  def get_psv
+    psv = []
+    psv[0] = self.id.to_s
+    psv[1] = self.username
+    psv[2] = self.fullname
+    psv[3] = self.nickname
+    psv[4] = self.irc_nick
+    psv[5] = self.email
+    psv[6] = self.webpage
+    psv[7] = self.flickr_username
+    psv[8] = self.yahooim_username
+    psv[9] = self.gtalk_username
+    psv[10] = self.visible
+    return "#{psv[0]}|#{psv[1]}|#{psv[2]}|#{psv[3]}|#{psv[4]}|#{psv[5]}|#{psv[6]}|#{psv[7]}|#{psv[8]}|#{psv[9]}|#{psv[10]}"
+  end
+
 
 end
