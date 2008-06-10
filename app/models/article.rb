cf1d1311b39fdd7df0e2f81b19927dc7d6561be7
Anurag <anurag@xinh.org>
2008-06-10 19:59:10 +0000

    Added validation checks.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :author, :foreign_key => "author_id", :class_name => "Person"
-  validates_presence_of :title, :body, :author_id, :created_at
+
+  validates_presence_of :body, :author_id, :created_at
+  validates_size_of     :title, :within => 1..150
+  validates_length_of   :body, :maximum => 10240, :message => "too long. Max length is 10KB"
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :venue
   has_many   :minutes
+
+  validates_presence_of :agenda, :venue_id, :date, :created_at
+  validates_size_of     :title, :within => 1..150
+  validates_length_of   :agenda, :maximum => 4086, :message => "too long. Max length is 4KB"
+  validates_length_of   :description, :maximum => 10240, :message => "too long. Max length is 10KB"
 end
